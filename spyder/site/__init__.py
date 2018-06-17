f49a682b400f9d7ff234da626c095a5139cc0986
Carlos Cordoba <ccordoba12@gmail.com>
2018-06-17 17:12:08 -0500

    Create a site package

---
+++ spyder/site/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © Spyder Project Contributors
+# Licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+
+"""
+Site package for the console kernel
+
+NOTE: This package shouldn't be imported at **any** place.
+      It's only used to set additional functionality for
+      our consoles.
+"""
