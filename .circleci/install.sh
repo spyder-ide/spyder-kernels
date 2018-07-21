4eb716866179116b60a987641d306f71ef21e284
Carlos Cordoba <ccordoba12@gmail.com>
2018-07-21 13:49:14 -0500

    Testing: Run tests in a bash script

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -7,5 +7,3 @@ export CONDA_DEPENDENCIES="ipykernel cloudpickle pytest"
 echo -e "PYTHON = $PYTHON_VERSION \n============"
 git clone git://github.com/astropy/ci-helpers.git > /dev/null
 source ci-helpers/travis/setup_conda_$TRAVIS_OS_NAME.sh
-export PATH="$HOME/miniconda/bin:$PATH"
-source activate test
