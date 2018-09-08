42f92336d4db3b24122523122cae8e8fae07c6f1
dalthviz <d.althviz10@uniandes.edu.co>
2018-09-08 23:30:05 +0200

    Mixin, Console, IPdb kernel Test: Test mixin class and refactor tests for the ConsoleKernel and IPdbKernel.

--- spyder_kernels/console/tests/test_console_kernel.py
+++ spyder_kernels/console/tests/test_console_kernel.py
@@ -11,18 +11,13 @@ Tests for the console kernel.
 
 # Standard library imports
 import os
-import os.path as osp
 
 # Test imports
 import pytest
 
 # Local imports
-from spyder_kernels.kernelmixin import PICKLE_PROTOCOL
-from spyder_kernels.utils.iofuncs import iofunctions
-from spyder_kernels.utils.test_utils import get_kernel, get_log_text
+from spyder_kernels.utils.test_utils import get_kernel
 
-# Third-party imports
-import cloudpickle
 
 # =============================================================================
 # Constants
@@ -34,7 +29,7 @@ FILES_PATH = os.path.dirname(os.path.realpath(__file__))
 # Fixtures
 # =============================================================================
 @pytest.fixture
-def kernel(request):
+def console_kernel(request):
     """Console kernel fixture"""
     # Get kernel instance
     kernel = get_kernel()
@@ -70,10 +65,10 @@ def kernel(request):
 # =============================================================================
 # Tests
 # =============================================================================
-def test_magics(kernel):
+def test_magics(console_kernel):
     """Check available magics in the kernel."""
-    line_magics = kernel.shell.magics_manager.magics['line']
-    cell_magics = kernel.shell.magics_manager.magics['cell']
+    line_magics = console_kernel.shell.magics_manager.magics['line']
+    cell_magics = console_kernel.shell.magics_manager.magics['cell']
     for magic in ['alias', 'alias_magic', 'autocall', 'automagic', 'autosave',
                   'bookmark', 'cd', 'clear', 'colors',
                   'config', 'connect_info', 'debug',
@@ -99,189 +94,6 @@ def test_magics(kernel):
                   'ruby', 'script', 'sh', 'svg', 'sx', 'system', 'time',
                   'timeit', 'writefile']:
         assert magic in cell_magics
-
-
-# --- For the Variable Explorer
-def test_get_namespace_view(kernel):
-    """
-    Test the namespace view of the kernel.
-    """
-    execute = kernel.do_execute('a = 1', True)
-    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
-                       'user_expressions': {}}
-    nsview = kernel.get_namespace_view()
-    assert "'a':" in nsview
-    assert "'type': 'int'" in nsview or "'type': u'int'" in nsview
-    assert "'size': 1" in nsview
-    assert "'color': '#0000ff'" in nsview
-    assert "'view': '1'" in nsview
-
-
-def test_get_var_properties(kernel):
-    """
-    Test the properties fo the variables in the namespace.
-    """
-    execute = kernel.do_execute('a = 1', True)
-    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
-                       'user_expressions': {}}
-    var_properties = kernel.get_var_properties()
-    assert "'a'" in var_properties
-    assert "'is_list': False" in var_properties
-    assert "'is_dict': False" in var_properties
-    assert "'len': None" in var_properties
-    assert "'is_array': False" in var_properties
-    assert "'is_image': False" in var_properties
-    assert "'is_data_frame': False" in var_properties
-    assert "'is_series': False" in var_properties
-    assert "'array_shape': None" in var_properties
-    assert "'array_ndim': None" in var_properties
-
-
-def test_send_spyder_msg(kernel):
-    """
-    Test publishing custom messages to the Spyder frontend.
-    """
-    spyder_msg_type, content, data = 'TEST', None, None
-    kernel.send_spyder_msg(spyder_msg_type, content, data)
-    log_text = get_log_text(kernel)
-    assert "{'spyder_msg_type': 'TEST'}" in log_text
-
-
-def test_get_value(kernel):
-    """Test getting the value of a variable."""
-    name = 'a'
-    execute = kernel.do_execute("a = 1", True)
-    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
-                       'user_expressions': {}}
-    # Check data type send
-    kernel.get_value(name)
-    log_text = get_log_text(kernel)
-    assert "{'spyder_msg_type': 'data'}" in log_text
-
-
-def test_set_value(kernel):
-    """Test setting the value of a variable."""
-    name = 'a'
-    execute = kernel.do_execute('a = 0', True)
-    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
-                       'user_expressions': {}}
-    value = [cloudpickle.dumps(10, protocol=PICKLE_PROTOCOL)]
-    PY2_frontend = False
-    kernel.set_value(name, value, PY2_frontend)
-    log_text = get_log_text(kernel)
-    assert "'__builtin__': <module " in log_text
-    assert "'__builtins__': <module " in log_text
-    assert "'_ih': ['']" in log_text
-    assert "'_oh': {}" in log_text
-    assert "'a': 10" in log_text
-
-
-def test_remove_value(kernel):
-    """Test the removal of a variable."""
-    name = 'a'
-    execute = kernel.do_execute('a = 1', True)
-    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
-                       'user_expressions': {}}
-    var_properties = kernel.get_var_properties()
-    assert "'a'" in var_properties
-    assert "'is_list': False" in var_properties
-    assert "'is_dict': False" in var_properties
-    assert "'len': None" in var_properties
-    assert "'is_array': False" in var_properties
-    assert "'is_image': False" in var_properties
-    assert "'is_data_frame': False" in var_properties
-    assert "'is_series': False" in var_properties
-    assert "'array_shape': None" in var_properties
-    assert "'array_ndim': None" in var_properties
-    kernel.remove_value(name)
-    var_properties = kernel.get_var_properties()
-    assert var_properties == '{}'
-
-
-def test_copy_value(kernel):
-    """Test the copy of a variable."""
-    orig_name = 'a'
-    new_name = 'b'
-    execute = kernel.do_execute('a = 1', True)
-    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
-                       'user_expressions': {}}
-    var_properties = kernel.get_var_properties()
-    assert "'a'" in var_properties
-    assert "'is_list': False" in var_properties
-    assert "'is_dict': False" in var_properties
-    assert "'len': None" in var_properties
-    assert "'is_array': False" in var_properties
-    assert "'is_image': False" in var_properties
-    assert "'is_data_frame': False" in var_properties
-    assert "'is_series': False" in var_properties
-    assert "'array_shape': None" in var_properties
-    assert "'array_ndim': None" in var_properties
-    kernel.copy_value(orig_name, new_name)
-    var_properties = kernel.get_var_properties()
-    assert "'a'" in var_properties
-    assert "'b'" in var_properties
-    assert "'is_list': False" in var_properties
-    assert "'is_dict': False" in var_properties
-    assert "'len': None" in var_properties
-    assert "'is_array': False" in var_properties
-    assert "'is_image': False" in var_properties
-    assert "'is_data_frame': False" in var_properties
-    assert "'is_series': False" in var_properties
-    assert "'array_shape': None" in var_properties
-    assert "'array_ndim': None" in var_properties
-
-
-def test_load_data(kernel):
-    """Test loading data from filename."""
-    namespace_file = osp.join(FILES_PATH, 'load_data.spydata')
-    extention = '.spydata'
-    kernel.load_data(namespace_file, extention)
-    var_properties = kernel.get_var_properties()
-    assert "'a'" in var_properties
-    assert "'is_list': False" in var_properties
-    assert "'is_dict': False" in var_properties
-    assert "'len': None" in var_properties
-    assert "'is_array': False" in var_properties
-    assert "'is_image': False" in var_properties
-    assert "'is_data_frame': False" in var_properties
-    assert "'is_series': False" in var_properties
-    assert "'array_shape': None" in var_properties
-    assert "'array_ndim': None" in var_properties
-
-
-def test_save_namespace(kernel):
-    """Test saving the namespace into filename."""
-    namespace_file = osp.join(FILES_PATH, 'save_data.spydata')
-    execute = kernel.do_execute('b = 1', True)
-    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
-                       'user_expressions': {}}
-    kernel.save_namespace(namespace_file)
-    assert osp.isfile(namespace_file)
-    load_func = iofunctions.load_funcs['.spydata']
-    data, error_message = load_func(namespace_file)
-    assert data == {'b': 1}
-    assert not error_message
-    os.remove(namespace_file)
-    assert not osp.isfile(namespace_file)
-
-
-# --- For the Help plugin
-def test_is_defined(kernel):
-    """Test method to tell if object is defined."""
-    obj = "debug"
-    assert kernel.is_defined(obj)
-
-
-def test_get_doc(kernel):
-    """Test to get object documentation dictionary."""
-    objtxt = 'help'
-    assert "Define the builtin 'help'" in kernel.get_doc(objtxt)['docstring']
-
-
-def test_get_source(kernel):
-    """Test to get object source."""
-    objtxt = 'help'
-    assert 'class _Helper(object):' in kernel.get_source(objtxt)
 
 
 if __name__ == "__main__":
---
+++ spyder_kernels/tests/test_kernelmixin.py
@@ -0,0 +1,389 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © 2018- Spyder Kernels Contributors
+# Licensed under the terms of the MIT License
+# (see spyder_kernels/__init__.py for details)
+# -----------------------------------------------------------------------------
+
+"""
+Tests for the console kernel.
+"""
+
+# Standard library imports
+import os
+import os.path as osp
+
+# Test imports
+import pytest
+
+# Local imports
+from spyder_kernels.kernelmixin import PICKLE_PROTOCOL
+from spyder_kernels.utils.iofuncs import iofunctions
+from spyder_kernels.utils.test_utils import get_log_text
+
+# Third-party imports
+import cloudpickle
+
+# =============================================================================
+# Constants
+# =============================================================================
+FILES_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+# =============================================================================
+# Tests
+# =============================================================================
+# --- For the Variable Explorer
+def test_get_namespace_view(console_kernel, ipdb_kernel):
+    """
+    Test the namespace view of the kernel.
+    """
+    # Console kernel
+    execute = console_kernel.do_execute('a = 1', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    nsview = console_kernel.get_namespace_view()
+    assert "'a':" in nsview
+    assert "'type': 'int'" in nsview or "'type': u'int'" in nsview
+    assert "'size': 1" in nsview
+    assert "'color': '#0000ff'" in nsview
+    assert "'view': '1'" in nsview
+
+    # IPdb kernel    
+    execute = ipdb_kernel.do_execute('a = 1', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    nsview = ipdb_kernel.get_namespace_view()
+    assert "'a':" in nsview
+    assert "'type': 'int'" in nsview or "'type': u'int'" in nsview
+    assert "'size': 1" in nsview
+    assert "'color': '#0000ff'" in nsview
+    assert "'view': '1'" in nsview
+
+
+def test_get_var_properties(console_kernel, ipdb_kernel):
+    """
+    Test the properties fo the variables in the namespace.
+    """
+    # Console kernel
+    execute = console_kernel.do_execute('a = 1', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    var_properties = console_kernel.get_var_properties()
+    assert "'a'" in var_properties
+    assert "'is_list': False" in var_properties
+    assert "'is_dict': False" in var_properties
+    assert "'len': None" in var_properties
+    assert "'is_array': False" in var_properties
+    assert "'is_image': False" in var_properties
+    assert "'is_data_frame': False" in var_properties
+    assert "'is_series': False" in var_properties
+    assert "'array_shape': None" in var_properties
+    assert "'array_ndim': None" in var_properties
+
+    # IPdb kernel
+    execute = ipdb_kernel.do_execute('a = 1', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    var_properties = ipdb_kernel.get_var_properties()
+    assert "'a'" in var_properties
+    assert "'is_list': False" in var_properties
+    assert "'is_dict': False" in var_properties
+    assert "'len': None" in var_properties
+    assert "'is_array': False" in var_properties
+    assert "'is_image': False" in var_properties
+    assert "'is_data_frame': False" in var_properties
+    assert "'is_series': False" in var_properties
+    assert "'array_shape': None" in var_properties
+    assert "'array_ndim': None" in var_properties    
+    
+
+
+def test_send_spyder_msg(console_kernel, ipdb_kernel):
+    """
+    Test publishing custom messages to the Spyder frontend.
+    """
+    spyder_msg_type, content, data = 'TEST', None, None
+
+    # Console kernel
+    console_kernel.send_spyder_msg(spyder_msg_type, content, data)
+    log_text = get_log_text(console_kernel)
+    assert "{'spyder_msg_type': 'TEST'}" in log_text
+
+    # IPdb kernel
+    ipdb_kernel.send_spyder_msg(spyder_msg_type, content, data)
+    log_text = get_log_text(ipdb_kernel)
+    assert "{'spyder_msg_type': 'TEST'}" in log_text
+
+
+
+def test_get_value(console_kernel, ipdb_kernel):
+    """Test getting the value of a variable."""
+    name = 'a'
+
+    # Console kernel
+    execute = console_kernel.do_execute("a = 1", True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    # Check data type send
+    console_kernel.get_value(name)
+    log_text = get_log_text(console_kernel)
+    assert "{'spyder_msg_type': 'data'}" in log_text
+
+    # IPdb kernel
+    execute = ipdb_kernel.do_execute("a = 1", True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    # Check data type send
+    console_kernel.get_value(name)
+    log_text = get_log_text(ipdb_kernel)
+    assert "{'spyder_msg_type': 'data'}" in log_text
+
+
+def test_set_value(console_kernel, ipdb_kernel):
+    """Test setting the value of a variable."""
+    name = 'a'
+
+    # Console kernel
+    execute = console_kernel.do_execute('a = 0', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    value = [cloudpickle.dumps(10, protocol=PICKLE_PROTOCOL)]
+    PY2_frontend = False
+    console_kernel.set_value(name, value, PY2_frontend)
+    log_text = get_log_text(console_kernel)
+    assert "'__builtin__': <module " in log_text
+    assert "'__builtins__': <module " in log_text
+    assert "'_ih': ['']" in log_text
+    assert "'_oh': {}" in log_text
+    assert "'a': 10" in log_text
+
+    # IPdb kernel
+    execute = ipdb_kernel.do_execute('a = 0', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    value = [cloudpickle.dumps(10, protocol=PICKLE_PROTOCOL)]
+    PY2_frontend = False
+    console_kernel.set_value(name, value, PY2_frontend)
+    log_text = get_log_text(ipdb_kernel)
+    assert "'__builtin__': <module " in log_text
+    assert "'__builtins__': <module " in log_text
+    assert "'_ih': ['']" in log_text
+    assert "'_oh': {}" in log_text
+    assert "'a': 10" in log_text
+
+
+def test_remove_value(console_kernel, ipdb_kernel):
+    """Test the removal of a variable."""
+    name = 'a'
+
+    # Console kernel
+    execute = console_kernel.do_execute('a = 1', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    var_properties = console_kernel.get_var_properties()
+    assert "'a'" in var_properties
+    assert "'is_list': False" in var_properties
+    assert "'is_dict': False" in var_properties
+    assert "'len': None" in var_properties
+    assert "'is_array': False" in var_properties
+    assert "'is_image': False" in var_properties
+    assert "'is_data_frame': False" in var_properties
+    assert "'is_series': False" in var_properties
+    assert "'array_shape': None" in var_properties
+    assert "'array_ndim': None" in var_properties
+    console_kernel.remove_value(name)
+    var_properties = console_kernel.get_var_properties()
+    assert var_properties == '{}'
+
+    # IPdb kernel
+    execute = ipdb_kernel.do_execute('a = 1', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    var_properties = ipdb_kernel.get_var_properties()
+    assert "'a'" in var_properties
+    assert "'is_list': False" in var_properties
+    assert "'is_dict': False" in var_properties
+    assert "'len': None" in var_properties
+    assert "'is_array': False" in var_properties
+    assert "'is_image': False" in var_properties
+    assert "'is_data_frame': False" in var_properties
+    assert "'is_series': False" in var_properties
+    assert "'array_shape': None" in var_properties
+    assert "'array_ndim': None" in var_properties
+    ipdb_kernel.remove_value(name)
+    var_properties = ipdb_kernel.get_var_properties()
+    assert var_properties == '{}'
+
+
+def test_copy_value(console_kernel, ipdb_kernel):
+    """Test the copy of a variable."""
+    orig_name = 'a'
+    new_name = 'b'
+
+    # Console kernel
+    execute = console_kernel.do_execute('a = 1', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    var_properties = console_kernel.get_var_properties()
+    assert "'a'" in var_properties
+    assert "'is_list': False" in var_properties
+    assert "'is_dict': False" in var_properties
+    assert "'len': None" in var_properties
+    assert "'is_array': False" in var_properties
+    assert "'is_image': False" in var_properties
+    assert "'is_data_frame': False" in var_properties
+    assert "'is_series': False" in var_properties
+    assert "'array_shape': None" in var_properties
+    assert "'array_ndim': None" in var_properties
+    console_kernel.copy_value(orig_name, new_name)
+    var_properties = console_kernel.get_var_properties()
+    assert "'a'" in var_properties
+    assert "'b'" in var_properties
+    assert "'is_list': False" in var_properties
+    assert "'is_dict': False" in var_properties
+    assert "'len': None" in var_properties
+    assert "'is_array': False" in var_properties
+    assert "'is_image': False" in var_properties
+    assert "'is_data_frame': False" in var_properties
+    assert "'is_series': False" in var_properties
+    assert "'array_shape': None" in var_properties
+    assert "'array_ndim': None" in var_properties
+
+    # IPdb kernel
+    execute = ipdb_kernel.do_execute('a = 1', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    var_properties = ipdb_kernel.get_var_properties()
+    assert "'a'" in var_properties
+    assert "'is_list': False" in var_properties
+    assert "'is_dict': False" in var_properties
+    assert "'len': None" in var_properties
+    assert "'is_array': False" in var_properties
+    assert "'is_image': False" in var_properties
+    assert "'is_data_frame': False" in var_properties
+    assert "'is_series': False" in var_properties
+    assert "'array_shape': None" in var_properties
+    assert "'array_ndim': None" in var_properties
+    ipdb_kernel.copy_value(orig_name, new_name)
+    var_properties = ipdb_kernel.get_var_properties()
+    assert "'a'" in var_properties
+    assert "'b'" in var_properties
+    assert "'is_list': False" in var_properties
+    assert "'is_dict': False" in var_properties
+    assert "'len': None" in var_properties
+    assert "'is_array': False" in var_properties
+    assert "'is_image': False" in var_properties
+    assert "'is_data_frame': False" in var_properties
+    assert "'is_series': False" in var_properties
+    assert "'array_shape': None" in var_properties
+    assert "'array_ndim': None" in var_properties    
+
+
+def test_load_data(console_kernel, ipdb_kernel):
+    """Test loading data from filename."""
+    namespace_file = osp.join(FILES_PATH, 'load_data.spydata')
+    extention = '.spydata'
+
+    # Console kernel
+    console_kernel.load_data(namespace_file, extention)
+    var_properties = console_kernel.get_var_properties()
+    assert "'a'" in var_properties
+    assert "'is_list': False" in var_properties
+    assert "'is_dict': False" in var_properties
+    assert "'len': None" in var_properties
+    assert "'is_array': False" in var_properties
+    assert "'is_image': False" in var_properties
+    assert "'is_data_frame': False" in var_properties
+    assert "'is_series': False" in var_properties
+    assert "'array_shape': None" in var_properties
+    assert "'array_ndim': None" in var_properties
+
+    # Console IPdb
+    ipdb_kernel.load_data(namespace_file, extention)
+    var_properties = ipdb_kernel.get_var_properties()
+    assert "'a'" in var_properties
+    assert "'is_list': False" in var_properties
+    assert "'is_dict': False" in var_properties
+    assert "'len': None" in var_properties
+    assert "'is_array': False" in var_properties
+    assert "'is_image': False" in var_properties
+    assert "'is_data_frame': False" in var_properties
+    assert "'is_series': False" in var_properties
+    assert "'array_shape': None" in var_properties
+    assert "'array_ndim': None" in var_properties
+
+
+
+def test_save_namespace(console_kernel, ipdb_kernel):
+    """Test saving the namespace into filename."""
+    namespace_file = osp.join(FILES_PATH, 'save_data.spydata')
+
+    # Console kernel
+    execute = console_kernel.do_execute('b = 1', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    console_kernel.save_namespace(namespace_file)
+    assert osp.isfile(namespace_file)
+    load_func = iofunctions.load_funcs['.spydata']
+    data, error_message = load_func(namespace_file)
+    assert data == {'b': 1}
+    assert not error_message
+    os.remove(namespace_file)
+    assert not osp.isfile(namespace_file)
+
+    # IPkernel kernel
+    execute = ipdb_kernel.do_execute('b = 1', True)
+    assert execute == {'execution_count': 0, 'payload': [], 'status': 'ok',
+                       'user_expressions': {}}
+    ipdb_kernel.save_namespace(namespace_file)
+    assert osp.isfile(namespace_file)
+    load_func = iofunctions.load_funcs['.spydata']
+    data, error_message = load_func(namespace_file)
+    assert data == {'b': 1}
+    assert not error_message
+    os.remove(namespace_file)
+    assert not osp.isfile(namespace_file)
+
+
+
+# --- For the Help plugin
+def test_is_defined(console_kernel, ipdb_kernel):
+    """Test method to tell if object is defined."""
+    obj = "debug"
+
+    # Console kernel
+    assert console_kernel.is_defined(obj)
+
+    # IPdb kernel
+    assert ipdb_kernel.is_defined(obj)
+
+
+
+def test_get_doc(console_kernel, ipdb_kernel):
+    """Test to get object documentation dictionary."""
+    objtxt = 'help'
+
+    # Console kernel
+    assert "Define the builtin 'help'" in console_kernel.get_doc(
+                                                        objtxt)['docstring']
+
+    # IPdb kernel
+    assert "Define the builtin 'help'" in ipdb_kernel.get_doc(
+                                                        objtxt)['docstring']
+
+
+def test_get_source(console_kernel, ipdb_kernel):
+    """Test to get object source."""
+    objtxt = 'help'
+
+    # Console kernel
+    assert 'class _Helper(object):' in console_kernel.get_source(objtxt)
+
+    # IPdb kernel
+    assert 'class _Helper(object):' in ipdb_kernel.get_source(objtxt)
+
+
+
+if __name__ == "__main__":
+    pytest.main()
