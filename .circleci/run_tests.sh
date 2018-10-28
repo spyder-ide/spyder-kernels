2285867a176f274c59a2f5eae0a5837c656912f5
Carlos Cordoba <ccordoba12@gmail.com>
2018-10-27 22:53:51 -0500

    Testing: Remove --cov-report=term-missing from pytest runs

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -3,7 +3,7 @@
 export PATH="$HOME/miniconda/bin:$PATH"
 source activate test
 
-pytest -x -vv --cov=spyder_kernels --cov-report=term-missing spyder_kernels
+pytest -x -vv --cov=spyder_kernels spyder_kernels
 
 if [ $? -ne 0 ]; then
     exit 1
