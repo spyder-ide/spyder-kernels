d0126bc6a2371912a991968702a14134810fc439
Carlos Cordoba <ccordoba12@gmail.com>
2018-12-22 14:28:31 -0500

    Testing: Drop using ci-helpers in Circle

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-export TRAVIS_OS_NAME="linux"
-export CONDA_DEPENDENCIES_FLAGS="--quiet"
-export CONDA_DEPENDENCIES="ipykernel cloudpickle nomkl numpy pandas scipy pytest pytest-cov mock"
-export PIP_DEPENDENCIES="codecov wurlitzer"
+# Install Miniconda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+source $HOME/miniconda/etc/profile.d/conda.sh
 
-echo -e "PYTHON = $PYTHON_VERSION \n============"
-git clone git://github.com/astropy/ci-helpers.git > /dev/null
-source ci-helpers/travis/setup_conda_$TRAVIS_OS_NAME.sh
+# Make new conda environment with required Python version
+conda create -y -n test python=$PYTHON_VERSION
+conda activate test
+
+# Install dependencies
+conda install -y -q --file requirements.txt
+
+# Install test dependencies
+conda install -y -q nomkl numpy pandas scipy pytest pytest-cov mock
+
+# Install codecov
+pip install -q codecov
