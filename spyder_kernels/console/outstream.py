0b0da205328d8c9882b356519b20224059d149b7
Pieter Eendebak <pieter.eendebak@gmail.com>
2021-07-27 09:40:25 +0200

    Update spyder_kernels/console/outstream.py
    Co-authored-by: yfprojects <62463991+real-yfprojects@users.noreply.github.com>

--- spyder_kernels/console/outstream.py
+++ spyder_kernels/console/outstream.py
@@ -12,6 +12,6 @@ Custom Spyder Outstream class.
 
 from ipykernel.iostream import OutStream
 class TTYOutStream(OutStream):
-        """ Subclass of OutStream that represents a TTY """
+        """Subclass of OutStream that represents a TTY."""
         def __init__(self, session, pub_thread, name, pipe=None, echo=None, *, watchfd=True):
             super().__init__(session, pub_thread, name, pipe, echo=echo,  watchfd=watchfd, isatty=True)
