f1fda30a866059dabbb328e511d1c1baebff75e9
Carlos Cordoba <ccordoba12@gmail.com>
2024-02-27 12:38:00 -0500

    Increase minimal required version of PyZMQ to 24.0.0
    This matches the one set in IPykernel.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ REQUIREMENTS = [
     'jupyter-client>=5.3.4,<6; python_version<"3"',
     'jupyter-client>=7.4.9,<9; python_version>="3"',
     'pyzmq>=17,<20; python_version<"3"',
-    'pyzmq>=22.1.0; python_version>="3"',
+    'pyzmq>=24.0.0; python_version>="3"',
     'wurlitzer>=1.0.3;platform_system!="Windows"',
 ]
 
