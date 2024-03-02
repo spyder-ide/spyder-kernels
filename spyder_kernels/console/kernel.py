fdecea3aa4d80823590beb460f773e288ecbfe06
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-03-01 21:53:04 -0800

    Clean up current working directory manipulations

--- spyder_kernels/console/kernel.py
+++ spyder_kernels/console/kernel.py
@@ -93,11 +93,6 @@ class SpyderKernel(IPythonKernel):
         # To save the python env info
         self.pythonenv_info: PythonEnvInfo = {}
 
-        # Re-add current working directory path into sys.path after
-        # removing it before kernel started
-        if '' not in sys.path:
-            sys.path.insert(0, '')
-
         # Store original sys.path. Kernels are started with PYTHONPATH
         # removed from environment variables, so this will never have
         # user paths and should be clean.
@@ -801,8 +796,9 @@ class SpyderKernel(IPythonKernel):
 
                 # Ensure current directory is always first to imitate Python
                 # standard behavior
-                sys.path.remove('')
-                sys.path.insert(0, '')
+                if '' in sys.path:
+                    sys.path.remove('')
+                    sys.path.insert(0, '')
             else:
                 sys.path[:] = self._sys_path + new_path
         else:
