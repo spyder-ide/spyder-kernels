14cb17b00707a4b9d9eadfc8f3afd3e4e4cf116a
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-03-01 19:11:04 -0800

    Ensure that the current working directory is re-added to sys.path after the kernels starts. This is removed prior to kernel startup to protect spyder-kernels from offending user modules, but should be put back after startup.

--- spyder_kernels/console/kernel.py
+++ spyder_kernels/console/kernel.py
@@ -76,13 +76,15 @@ class SpyderKernel(IPythonKernel):
         # Socket to signal shell_stream locally
         self.loopback_socket = None
 
+        # Re-add current working directory path into sys.path after
+        # removing it before kernel started
+        if '' not in sys.path:
+            sys.path.insert(0, '')
+
         # Store original sys.path. Kernels are started with PYTHONPATH
         # removed from environment variables, so this will never have
         # user paths and should be clean.
         self._sys_path = sys.path.copy()
-        if '' in self._sys_path:
-            self._sys_path.remove('')
-        self._sys_path.insert(0, '')
 
     @property
     def kernel_info(self):
@@ -791,10 +793,10 @@ class SpyderKernel(IPythonKernel):
             if prioritize:
                 sys.path[:] = new_path + self._sys_path
 
-                # Ensure current directory is always first
-                if '' in sys.path:
-                    sys.path.remove('')
-                    sys.path.insert(0, '')
+                # Ensure current directory is always first to imitate Python
+                # standard behavior
+                sys.path.remove('')
+                sys.path.insert(0, '')
             else:
                 sys.path[:] = self._sys_path + new_path
         else:
